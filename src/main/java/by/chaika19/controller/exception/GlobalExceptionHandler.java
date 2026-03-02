00d53006d689c83dd4a4557523e3096dc06c8660
Chuiko Egor <egorchuiko7@gmail.com>
2026-03-02 18:58:54 +0300

    added 403, 404, 500 (and any other) error handling

--- src/main/java/by/chaika19/controller/exception/GlobalExceptionHandler.java
+++ src/main/java/by/chaika19/controller/exception/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package by.chaika19.controller.exception;
 
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.webmvc.error.ErrorController;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,12 +13,33 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class GlobalExceptionHandler implements ErrorController {
 
     @RequestMapping("/error")
-    public String getErrorPage() {
-        return "public/error/error-page";
+    public String redirectToSpecificErrorPage(HttpServletResponse response) {
+        return switch (HttpStatus.valueOf(response.getStatus())) {
+            case FORBIDDEN -> "redirect:/error/403";
+            case NOT_FOUND -> "redirect:/error/404";
+            default -> "redirect:/error/500";
+        };
     }
 
-    @ExceptionHandler(Throwable.class)
-    public String handleThrowable() {
-        return "redirect:/error";
+    @RequestMapping("/error/500")
+    public String getCommonErrorPage() {
+        return "public/error/common-error-page";
+    }
+
+    @RequestMapping("/error/403")
+    public String getForbiddenErrorPage() {
+        return "public/error/forbidden-error-page";
+    }
+
+    @RequestMapping("/error/404")
+    public String getNotFoundErrorPage() {
+        return "public/error/not-found-error-page";
+    }
+
+    @ExceptionHandler(org.springframework.dao.DataAccessException.class)
+    public String handleDatabaseError(Exception ex) {
+        ex.printStackTrace();
+        return "redirect:/error/500";
     }
 }
+
